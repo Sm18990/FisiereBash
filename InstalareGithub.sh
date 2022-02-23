e71d78507cf7e4c5f8c2324ec426c8b5b7d57bf7
Sm18990 <94412232+Sm18990@users.noreply.github.com>
2022-02-24 01:56:48 +0200

    Create InstalareGithub.sh

---
+++ InstalareGithub.sh
@@ -0,0 +1,12 @@
+!#/bin/bash/
+
+sudo apt update
+
+sudo apt upgrade
+
+sudo apt-get install gdebi-core
+
+sudo wget https://github.com/shiftkey/desktop/releases/download/release-2.9.3-linux3/GitHubDesktop-linux-2.9.3-linux3.deb
+
+sudo gdebi GitHubDesktop-linux-2.9.3-linux3.deb
+
