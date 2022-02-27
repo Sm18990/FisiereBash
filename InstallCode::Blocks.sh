a087cb28b8719884e0bd56643e3416ce4f111d08
Moscalu Stefan <94412232+Sm18990@users.noreply.github.com>
2022-02-27 22:50:31 +0200

    .

---
+++ InstallCode::Blocks.sh
@@ -0,0 +1,15 @@
+!#/bin/bash  
+
+sudo apt update
+
+sudo apt upgrade
+
+sudo apt install build-essential
+
+sudo apt-get install manpages-dev
+
+sudo apt install codeblocks
+
+codeblocks
+
+
---
+++ InstallGit.sh
@@ -0,0 +1,11 @@
+!#/bin/bash/
+
+sudo apt update 
+
+sudo apt upgrade 
+
+sudo apt-get install git
+
+git --version
+
+git
