7d1aeb53dae28b28c2ebaa886d0a123d3e0542fd
Sm18990 <94412232+Sm18990@users.noreply.github.com>
2022-02-27 17:35:14 +0200

    update

---
+++ InstalareChrome.sh
@@ -0,0 +1,11 @@
+!#/bin/bash/
+
+sudo apt update
+
+sudo apt upgrade
+
+sudo apt install wget
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome-stable_current_amd64.deb
