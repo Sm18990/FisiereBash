ec2ff229e6eed64268b623c6cc838d623b5f5a2b
Moscalu Stefan <94412232+Sm18990@users.noreply.github.com>
2022-02-28 21:17:43 +0200

    Create InstallOpera.sh

---
+++ InstallOpera.sh
@@ -0,0 +1,15 @@
+!#/bin/bash/
+
+sudo apt update
+
+sudo apt upgrade
+
+sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
+
+sudo wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=i386,amd64] https://deb.opera.com/opera-stable/ stable non-free"
+
+sudo apt install opera-stable
+
+opera
