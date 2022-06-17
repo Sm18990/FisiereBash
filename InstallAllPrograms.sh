b925ddf73b41c913b2529e5b5df9ee8dde8978c6
Moscalu Stefan <94412232+Sm18990@users.noreply.github.com>
2022-06-17 22:48:29 +0300

    Update

--- InstallAllPrograms.sh
+++ InstallAllPrograms.sh
@@ -1,41 +1,24 @@
 !#/bin/bash/
 
 sudo apt update
-
 sudo apt upgrade
-
-sudp apt install -y gdebi-core
-
-sudo apt install -y gcc
-
-sudo apt install -y g++
-
-sudo apt install -y snapd
-
-sudo snap install -y code --classic
-
+sudp apt install  gdebi-core
+sudo apt-get install -y gcc
+sudo apt-get install -y g++
+sudo apt-get install -y snapd
+sudo snap install code --classic
 sudo wget https://github.com/shiftkey/desktop/releases/download/release-2.9.3-linux3/GitHubDesktop-linux-2.9.3-linux3.deb
-
 sudo gdebi GitHubDesktop-linux-2.9.3-linux3.deb
-
 rm  GitHubDesktop-linux-2.9.3-linux3.deb
-
+sudo apt-get install jq
+sudo apt-get install curl
+sudo apt-get install kio-gdrive
+sudo apt-get install nodejs
 sudo snap install discord
-
 sudo snap install zoom-client
-
-sudo apt install -y ssh
-
-sudo apt install gufw
-
+sudo apt-get install -y ssh
+sudo apt-get install gufw
 sudo snap install ngrok
-
-sudo apt install default-jdk
-
-sudo apt update
-
-sudo apt install default-jre
-
-wget https://www.jetbrains.com/toolbox-app/download/download-thanks.html?platform=linux
-
-sudo apt install virtualbox-qt
+sudo apt-get install default-jdk
+sudo apt-get install default-jre
+sudo apt-get install virtualbox-qt
